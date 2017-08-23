cfa79673da56dea3f7da834c1cb5087d6dec82ba
ameer alalawi <asalalawi@gmail.com>
2017-08-23 20:52:08 +0100

    backend core logic

--- app/models/case.rb
+++ app/models/case.rb
@@ -17,8 +17,34 @@ class Case < ApplicationRecord
   validates :output_pref_6, inclusion: { in: [true, false] }
 
 
-    def default_values
+  def default_values
     self.status ||= 'unpublished' # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
   end
 
+  def build
+    extents = determine_extents
+    mycase = {}
+    mycase[:A] = {}
+    mycase[:B] = {}
+    mycase[:xaxis] = (extents[:absstart]..extents[:absend]).map { |date| (date).strftime('%Y%m')}.uniq
+
+    self.lines.each do |line|
+      if line.scenario == "A"
+        mycase[:A].merge!(line.build(extents))
+      elsif line.scenario == "B"
+        mycase[:B].merge!(line.build(extents))
+      end
+    end
+    return mycase
+  end
+
+  def determine_extents
+    m_start = self.lines.first.start_date
+    m_end = self.lines.first.end_date
+    self.lines.each do |line|
+      line.start_date < m_start ? m_start = line.start_date : nil
+      line.end_date > m_end ? m_end = line.end_date : nil
+    end
+    return {absstart: m_start, absend: m_end, months:(m_end.year * 12 + m_end.month) - (m_start.year * 12 + m_start.month)}
+  end
 end
--- app/models/line.rb
+++ app/models/line.rb
@@ -12,10 +12,32 @@ class Line < ApplicationRecord
   validates :end_date, presence: true
   validates :escalator, presence: true
 
-   def default_values
+  def default_values
     self.escalator ||= 0 # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
   end
 
+  def build(extents)
+    # Initialize this line to its own extents
+    lineext = self.recurrence == "oneoff" ? 1 : (self.end_date.year * 12 + self.end_date.month) - (self.start_date.year * 12 + self.start_date.month)
+    myline = Array.new(lineext, 0)
+    # Load line characteristics
+    category = self.category == "cost" ? -1 : 1
+    arr = ["oneoff", "monthly", "quaterly", "yearly"]
+    recurrence = [9999,1,4,12][arr.size.times.select {|i| arr[i] == self.recurrence}.first]
+    effectiveesc = ((1 + 0.02)**(1/recurrence))-1
+    variable_value = self.variable.eval_var
 
+    # Fill line
+    idx = 0
+    while idx <= lineext-1
+      myline[idx] = variable_value
+      variable_value *= (1 + effectiveesc)
+      idx += recurrence
+    end
+    payload = Array.new(extents[:months], 0)
+    offset = (self.start_date.year * 12 + self.start_date.month) - (extents[:absstart].year * 12 + extents[:absstart].month)
+    payload[offset,lineext] = myline
+    return {self.name.to_sym => payload}
+  end
 
 end
