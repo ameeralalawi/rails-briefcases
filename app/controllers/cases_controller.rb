c9f13b94f2a05bd06dfce147a002b17e7a2821ec
ameer alalawi <asalalawi@gmail.com>
2017-08-27 23:39:26 +0100

    fixed some save buttons and some publish logic

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,5 +1,12 @@
 class CasesController < ApplicationController
-  def show
+  skip_before_action :authenticate_user!, only: :show
 
+  def show
+    @case = Case.find(params[:id])
+    unless @case.status == "published"
+      flash[:notice] = "This case has not been published"
+      redirect_to root_path
+      return
+    end
   end
 end
