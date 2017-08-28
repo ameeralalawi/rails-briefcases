7c877c8a5d2b7cda8e866d03d7bc8a8cb27913d2
ameer alalawi <asalalawi@gmail.com>
2017-08-28 19:22:13 +0100

    minor fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require jquery-ui
-//= require bootstrap-datepicker
+//= require bootstrap-datepicker/core
 //= require fullpage
 //= require clipboard
 //= require_tree .
