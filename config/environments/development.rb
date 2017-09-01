604a58390306455ea9c8aaf6e9fa46ac65a92c0e
ameer alalawi <asalalawi@gmail.com>
2017-09-01 11:17:27 +0100

    small fix 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.action_controller.default_url_options = { host: 'briefcases.io'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -52,6 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_controller.default_url_options[:host] = 'briefcases.io'
-  config.action_mailer.default_url_options = { host: 'briefcases.io', port: 3000 }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
